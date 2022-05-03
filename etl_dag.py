420e1532fc44366ed8747a992f1d462f1ea974d4
Yuriy Savchuk <aruy.savchuk@gmail.com>
2022-05-03 22:59:04 +0300

    Add task_id which uploads file to S3

--- etl_dag.py
+++ etl_dag.py
@@ -9,6 +9,14 @@ from datetime import datetime
 from Spark_process import Spark_process
 
 
+
+def upload_to_s3(filename, key, bucket_name):
+    from airflow.hooks.S3_hook import S3Hook
+    hook = S3Hook('s3_conn')
+    hook.load_file(filename, key, bucket_name)
+
+
+
 with DAG(
     dag_id='etl_dag',
     start_date=datetime(year=2022, month=5, day=1),
@@ -41,23 +49,32 @@ with DAG(
         python_callable=Spark_process.transform, 
         op_args=['/home/yu_savchuk/airflow/dags/title.basics.tsv',
                  '/home/yu_savchuk/airflow/dags/title.ratings.tsv'])
-        
-    task_delete_title=BashOperator(
-        task_id='delete_title',
-        bash_command='rm /home/yu_savchuk/airflow/dags/title.basics.tsv')
     
-    task_delete_rating=BashOperator(
+    task_delete_tsv_files=BashOperator(
         task_id='delete_rating',
-        bash_command='rm /home/yu_savchuk/airflow/dags/title.ratings.tsv')
-    
-    
-    task_download_title >> task_download_rating >> \
-        [task_unzip_rating, task_unzip_title] >> task_transform_files >> \
-            [task_delete_title, task_delete_rating]
+        bash_command='rm /home/yu_savchuk/airflow/dags/*.tsv')
     
 
     
+    task_download_title >> task_download_rating >> \
+        [task_unzip_rating, task_unzip_title] >> task_transform_files >> \
+            task_delete_tsv_files
     
+    if os.path.exists('/home/yu_savchuk/airflow/dags/title_rating.parquet'):
+        files=os.listdir('/home/yu_savchuk/airflow/dags/title_rating.parquet')
+        for file in files:
+            task_upload_to_s3=PythonOperator(
+                task_id=f'upload_to_s3_{file}',
+                python_callable=upload_to_s3,
+                op_kwargs={
+                    'filename': f'/home/yu_savchuk/airflow/dags/{file}',
+                    'key': f'title_rating.parquet/{file}',
+                    'bucket_name': 'imdb-bucket-etl'})
+            
+        task_delete_tsv_files >> task_upload_to_s3
+    else:
+        pass
+        
     
     
     
